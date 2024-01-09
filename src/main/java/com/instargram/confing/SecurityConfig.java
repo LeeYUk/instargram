abf4213d585854f62011abee85c3af8872640ee3
LeeYUk <eyedyd@naver.com>
2024-01-10 04:41:06 +0900

    AuthController.java와 SecurityConfig.java에 signup 기능추가, signup.mustache파일생성

--- src/main/java/com/instargram/confing/SecurityConfig.java
+++ src/main/java/com/instargram/confing/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -16,6 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         //인증이 되지않은 모든 사용자는 로그인페이지로 이동
+        http.csrf(AbstractHttpConfigurer::disable);
         http
                 .authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/**", "/css/**", "/images/**", "/js/**").permitAll()//해당 요청은 허용
                         .anyRequest().authenticated())//설정된 값들 이외 나머지 URL은 인증된 접근 가능
--- src/main/java/com/instargram/domain/auth/AuthController.java
+++ src/main/java/com/instargram/domain/auth/AuthController.java
@@ -2,6 +2,7 @@ package com.instargram.domain.auth;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
@@ -10,5 +11,17 @@ public class AuthController {
     public String loginPage(){
 
         return "auth/login";
+    }
+
+    @GetMapping("/auth/signup")
+    public String signupPage(){
+
+        return "auth/signup";//회원가입페이지로 이동하는 요청
+    }
+
+    @PostMapping("/auth/signup")
+    public String signup(){
+
+        return "auth/login"; //회원가입이 완료되면 로그인 페이지로 이동한다.
     }
 }
