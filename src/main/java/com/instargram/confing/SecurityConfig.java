f0c6f91c3a5814e895ef671b0bda66035d5da82b
LeeYUk <eyedyd@naver.com>
2024-01-10 03:42:51 +0900

    Initial commit

---
+++ src/main/java/com/instargram/confing/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.instargram.confing;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@EnableWebSecurity//해당 파일로 시큐리티를 활성화
+@Configuration//IOC
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //인증이 되지않은 모든 사용자는 로그인페이지로 이동
+        http
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/**", "/css/**", "/images/**", "/js/**").permitAll()//해당 요청은 허용
+                        .anyRequest().authenticated())//설정된 값들 이외 나머지 URL은 인증된 접근 가능
+                .formLogin(formLogin->formLogin
+                        .loginPage("/auth/login")//GET
+                        .defaultSuccessUrl("/"));
+
+        return http.build();
+    }
+}
---
+++ src/test/java/com/instargram/instargram/InstargramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instargram.instargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstargramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
